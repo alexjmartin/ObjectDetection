fd095bf1ec3466dab8021b106841a62d61e9bf13
alexjmartin <83616180+alexjmartin@users.noreply.github.com>
2021-06-29 22:10:41 +0100

    Create prep_and_train.py
    Initially created for training - need to add test and evaluation parts

---
+++ prep_and_train.py
@@ -0,0 +1,273 @@
+## Convert Training, test and validation files to Tensors from tfrecords
+from object_detection.utils import label_map_util
+import tensorflow as tf
+import numpy as np
+
+workspace = '{}'
+
+filenames = workspace+'tfrecords/train/F1-Car.tfrecord'
+raw_dataset = tf.data.TFRecordDataset(filenames)
+
+raw_image_dataset = raw_dataset
+
+# Create a dictionary describing the features.
+image_feature_description = {
+      'image/height': tf.io.FixedLenFeature([], tf.int64),
+     'image/object/bbox/ymin': tf.io.VarLenFeature(tf.float32),
+      'image/object/class/text': tf.io.VarLenFeature(tf.string),
+      'image/encoded': tf.io.FixedLenFeature([], tf.string),
+     'image/filename': tf.io.FixedLenFeature([], tf.string),
+      'image/object/bbox/ymax': tf.io.VarLenFeature(tf.float32),
+      'image/object/bbox/xmin': tf.io.VarLenFeature(tf.float32),
+      'image/width': tf.io.FixedLenFeature([], tf.int64),
+      'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+      'image/object/bbox/xmax': tf.io.VarLenFeature(tf.float32),
+}
+
+def _parse_image_function(example_proto):
+  # Parse the input tf.train.Example proto using the dictionary above.
+  return tf.io.parse_single_example(example_proto, image_feature_description)
+
+parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
+parsed_image_dataset
+
+test_filenames = workspace+'tfrecords/test/F1-Car.tfrecord'
+test_dataset = tf.data.TFRecordDataset(test_filenames)
+
+test_image_dataset = test_dataset
+
+parsed_test_image_dataset = test_image_dataset.map(_parse_image_function)
+parsed_test_image_dataset
+
+valid_filenames = workspace+'tfrecords/valid/F1-Car.tfrecord'
+valid_dataset = tf.data.TFRecordDataset(valid_filenames)
+
+valid_image_dataset = valid_dataset
+
+parsed_valid_image_dataset = valid_image_dataset.map(_parse_image_function)
+parsed_valid_image_dataset
+
+gt_boxes = []
+
+train_images_np = []
+
+for img in parsed_image_dataset:
+    label_num = len(img['image/object/bbox/ymin'].values.numpy())
+    for x in range(label_num):
+        box = np.array([[img['image/object/bbox/ymin'].values.numpy()[x], img['image/object/bbox/xmin'].values.numpy()[x], img['image/object/bbox/ymax'].values.numpy()[x], img['image/object/bbox/xmax'].values.numpy()[x]]])
+        gt_boxes.append(box)
+        img_tensor = tf.io.decode_image(
+        img['image/encoded'].numpy(), channels=None, dtype=tf.dtypes.uint8, name=None, expand_animations=True).numpy()
+        train_images_np.append(img_tensor)
+        
+# By convention, our non-background classes start counting at 1.  Given
+# that we will be predicting just one class, we will therefore assign it a
+# `class id` of 1.
+num_classes = 1
+
+PATH_TO_LABELS = 'C:/datasets/TensorFlow/workspace/training_demo/annotations/simple_label_index.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+category_index = category_index
+# Convert class labels to one-hot; convert everything to tensors.
+# The `label_id_offset` here shifts all classes by a certain number of indices;
+# we do this here so that the model receives one-hot labels where non-background
+# classes start counting at the zeroth index.  This is ordinarily just handled
+# automatically in our training binaries, but we need to reproduce it here.
+label_id_offset = 1
+train_image_tensors = []
+gt_classes_one_hot_tensors = []
+gt_box_tensors = []
+for (train_image_np, gt_box_np) in zip(
+    train_images_np, gt_boxes):
+  train_image_tensors.append(tf.expand_dims(tf.convert_to_tensor(
+      train_image_np, dtype=tf.float32), axis=0))
+  gt_box_tensors.append(tf.convert_to_tensor(gt_box_np, dtype=tf.float32))
+  zero_indexed_groundtruth_classes = tf.convert_to_tensor(
+      np.ones(shape=[gt_box_np.shape[0]], dtype=np.int32) - label_id_offset)
+  gt_classes_one_hot_tensors.append(tf.one_hot(
+      zero_indexed_groundtruth_classes, num_classes))
+print('Done prepping data.')
+
+
+def plot_detections(image_np,
+                    boxes,
+                    classes,
+                    scores,
+                    category_index,
+                    figsize=(12, 12),
+                    image_name=None):
+  """Wrapper function to visualize detections.
+
+  Args:
+    image_np: uint8 numpy array with shape (img_height, img_width, 3)
+    boxes: a numpy array of shape [N, 4]
+    classes: a numpy array of shape [N]. Note that class indices are 1-based,
+      and match the keys in the label map.
+    scores: a numpy array of shape [N] or None.  If scores=None, then
+      this function assumes that the boxes to be plotted are groundtruth
+      boxes and plot all boxes as black with no classes or scores.
+    category_index: a dict containing category dictionaries (each holding
+      category index `id` and category name `name`) keyed by category indices.
+    figsize: size for the figure.
+    image_name: a name for the image file.
+  """
+  image_np_with_annotations = image_np.copy()
+  viz_utils.visualize_boxes_and_labels_on_image_array(
+      image_np_with_annotations,
+      boxes,
+      classes,
+      scores,
+      category_index,
+      use_normalized_coordinates=True,
+      min_score_thresh=0.75)
+  if image_name:
+    plt.imsave(image_name, image_np_with_annotations)
+  else:
+    plt.imshow(image_np_with_annotations )
+    
+tf.keras.backend.clear_session()
+
+print('Building model and restoring weights for fine-tuning...', flush=True)
+num_classes = 1
+pipeline_config = workspace+'models/my_ssd_resnet50_v1_fpn/pipeline.config'
+checkpoint_path = workspace+'pre-trained-models/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8/checkpoint/ckpt-0'
+
+# Load pipeline config and build a detection model.
+#
+# Since we are working off of a COCO architecture which predicts 90
+# class slots by default, we override the `num_classes` field here to be just
+# one (for our F1 car class).
+configs = config_util.get_configs_from_pipeline_file(pipeline_config)
+model_config = configs['model']
+model_config.ssd.num_classes = num_classes
+model_config.ssd.freeze_batchnorm = True
+detection_model = model_builder.build(
+      model_config=model_config, is_training=True)
+
+# Set up object-based checkpoint restore --- RetinaNet has two prediction
+# `heads` --- one for classification, the other for box regression.  We will
+# restore the box regression head but initialize the classification head
+# from scratch (we show the omission below by commenting out the line that
+# we would add if we wanted to restore both heads)
+fake_box_predictor = tf.compat.v2.train.Checkpoint(
+    _base_tower_layers_for_heads=detection_model._box_predictor._base_tower_layers_for_heads,
+    # _prediction_heads=detection_model._box_predictor._prediction_heads,
+    #    (i.e., the classification head that we *will not* restore)
+    _box_prediction_head=detection_model._box_predictor._box_prediction_head,
+    )
+fake_model = tf.compat.v2.train.Checkpoint(
+          _feature_extractor=detection_model._feature_extractor,
+          _box_predictor=fake_box_predictor)
+ckpt = tf.compat.v2.train.Checkpoint(model=fake_model)
+ckpt.restore(checkpoint_path).expect_partial()
+
+# Run model through a dummy image so that variables are created
+image, shapes = detection_model.preprocess(tf.zeros([1, 416, 416, 3]))
+prediction_dict = detection_model.predict(image, shapes)
+_ = detection_model.postprocess(prediction_dict, shapes)
+print('Weights restored!')
+
+import datetime
+from tensorflow.keras import mixed_precision
+
+train_log_dir = 'C:/datasets/TensorFlow/workspace//training_demo/Tensorboard/logs'
+
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+
+# policy = mixed_precision.Policy('mixed_float16')
+# mixed_precision.set_global_policy(policy)
+
+tf.keras.backend.set_learning_phase(True)
+
+train_loss = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('train_accuracy')
+
+batch_size = 4
+learning_rate = 0.01
+num_batches = 100
+
+# Select variables in top layers to fine-tune.
+trainable_variables = detection_model.trainable_variables
+to_fine_tune = []
+prefixes_to_train = [
+  'WeightSharedConvolutionalBoxPredictor/WeightSharedConvolutionalBoxHead',
+  'WeightSharedConvolutionalBoxPredictor/WeightSharedConvolutionalClassHead']
+for var in trainable_variables:
+  if any([var.name.startswith(prefix) for prefix in prefixes_to_train]):
+    to_fine_tune.append(var)
+
+# Set up forward + backward pass for a single train step.
+def get_model_train_step_function(model, optimizer, vars_to_fine_tune):
+  """Get a tf.function for training step."""
+
+  # Use tf.function for a bit of speed.
+  # Comment out the tf.function decorator if you want the inside of the
+  # function to run eagerly.
+  @tf.function
+  def train_step_fn(image_tensors,
+                    groundtruth_boxes_list,
+                    groundtruth_classes_list):
+    """A single training iteration.
+
+    Args:
+      image_tensors: A list of [1, height, width, 3] Tensor of type tf.float32.
+        Note that the height and width can vary across images, as they are
+        reshaped within this function to be 416x416.
+      groundtruth_boxes_list: A list of Tensors of shape [N_i, 4] with type
+        tf.float32 representing groundtruth boxes for each image in the batch.
+      groundtruth_classes_list: A list of Tensors of shape [N_i, num_classes]
+        with type tf.float32 representing groundtruth boxes for each image in
+        the batch.
+
+    Returns:
+      A scalar tensor representing the total loss for the input batch.
+    """
+    shapes = tf.constant(batch_size * [[416, 416, 3]], dtype=tf.int32)
+    model.provide_groundtruth(
+        groundtruth_boxes_list=groundtruth_boxes_list,
+        groundtruth_classes_list=groundtruth_classes_list)
+    with tf.GradientTape() as tape:
+      preprocessed_images = tf.concat(
+          [detection_model.preprocess(image_tensor)[0]
+           for image_tensor in image_tensors], axis=0)
+      prediction_dict = model.predict(preprocessed_images, shapes)
+      losses_dict = model.loss(prediction_dict, shapes)
+      total_loss = losses_dict['Loss/localization_loss'] + losses_dict['Loss/classification_loss']
+      gradients = tape.gradient(total_loss, vars_to_fine_tune)
+      optimizer.apply_gradients(zip(gradients, vars_to_fine_tune))
+    return total_loss
+
+  return train_step_fn
+
+optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9)
+train_step_fn = get_model_train_step_function(
+    detection_model, optimizer, to_fine_tune)
+
+print('Start fine-tuning!', flush=True)
+for idx in range(num_batches):
+  # Grab keys for a random subset of examples
+  all_keys = list(range(len(train_images_np)))
+  random.shuffle(all_keys)
+  example_keys = all_keys[:batch_size]
+
+  # Note that we do not do data augmentation in this demo.  If you want a
+  # a fun exercise, we recommend experimenting with random horizontal flipping
+  # and random cropping :)
+  gt_boxes_list = [gt_box_tensors[key] for key in example_keys]
+  gt_classes_list = [gt_classes_one_hot_tensors[key] for key in example_keys]
+  image_tensors = [train_image_tensors[key] for key in example_keys]
+
+  # Training step (forward pass + backwards pass)
+  total_loss = train_step_fn(image_tensors, gt_boxes_list, gt_classes_list)
+    
+#   with train_summary_writer.as_default():
+#       tf.summary.scalar('loss', total_loss.result(), step=idx)
+#       #tf.summary.scalar('accuracy', train_accuracy.result(), step=epoch)
+
+  if idx % 10 == 0:
+    now = datetime.datetime.now().strftime("%H:%M:%S")
+    print(str(now) + ' batch ' + str(idx) + ' of ' + str(num_batches)
+    + ', loss=' +  str(total_loss.numpy()), flush=True)
+    
+print('Done fine-tuning!')
